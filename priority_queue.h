240087de5822aedfccdc595d53e3a8607c5e57d3
hsang-choi <51347243+hsang-choi@users.noreply.github.com>
2019-06-12 14:58:13 +0900

    Update priority_queue.h

--- priority_queue.h
+++ priority_queue.h
@@ -5,16 +5,16 @@
 #include <queue>
 
 template <typename T> 
-class templatePriorityQueue {
-	private:
-		std::priority_queue<T> storage;
+class PriorityQueue {
 	public:
-		templatePriorityQueue();
-		~templatePriorityQueue();
+		PriorityQueue();
+		~PriorityQueue();
 		bool empty() const;
 		const T& top() const;
 		int size();
 		void push(const T&);
 		void pop();	
+	private:
+		std::priority_queue<T> storage;
 };
 #endif //PRIORITY_QUEUE_H
